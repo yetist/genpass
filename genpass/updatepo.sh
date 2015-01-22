e11c7d73a4773b2e1b7d796f5100047fda3c72b9
yetist <xiaotian.wu@i-soft.com.cn>
2015-01-22 16:24:43 +0800

    update ui

--- genpass/updatepo.sh
+++ genpass/updatepo.sh
@@ -9,7 +9,7 @@ update_sources() {
 		xgettext -s -i -L C++ --from-code=utf-8 -d $DOMAIN -o ${DOMAIN}_src.pot $KEYWORDS $src
 		if [ -f ${DOMAIN}_src.pot ];then
 			[ -f ${DOMAIN}.pot ] || cp ${DOMAIN}_src.pot ${DOMAIN}.pot 
-			msgcat ${DOMAIN}.pot ${DOMAIN}_src.pot -o ${DOMAIN}.pot
+			msgcat -s ${DOMAIN}.pot ${DOMAIN}_src.pot -o ${DOMAIN}.pot
 			rm -f ${DOMAIN}_src.pot
 		fi
 	done
@@ -21,7 +21,7 @@ update_tmpl() {
 		xgettext -s -i -L Python --from-code=utf-8 -d $DOMAIN -o ${DOMAIN}_tmpl.pot $KEYWORDS $tmpl
 		if [ -f ${DOMAIN}_tmpl.pot ];then
 			[ -f ${DOMAIN}.pot ] || cp ${DOMAIN}_tmpl.pot ${DOMAIN}.pot 
-			msgcat ${DOMAIN}.pot ${DOMAIN}_tmpl.pot -o ${DOMAIN}.pot
+			msgcat -s ${DOMAIN}.pot ${DOMAIN}_tmpl.pot -o ${DOMAIN}.pot
 			rm -f ${DOMAIN}_tmpl.pot
 		fi
 	done
@@ -31,7 +31,7 @@ update_po() {
 	for l in ${LANGS[@]}; do
 		[ -d locale/$l/LC_MESSAGES ] || mkdir -p locale/$l/LC_MESSAGES
 		if [ -f locale/$l/LC_MESSAGES/${DOMAIN}.po ]; then
-			msgmerge -U locale/$l/LC_MESSAGES/${DOMAIN}.po ${DOMAIN}.pot
+			msgmerge -s -U locale/$l/LC_MESSAGES/${DOMAIN}.po ${DOMAIN}.pot
 		else
 			msginit -l ${l}.utf8 -o locale/$l/LC_MESSAGES/${DOMAIN}.po -i ${DOMAIN}.pot
 		fi
